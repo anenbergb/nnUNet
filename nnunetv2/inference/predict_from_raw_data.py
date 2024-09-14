24ca96d5dba5adfdd89a43ae91eb1b718c6a01d6
Bryan Anenberg <anenbergb@gmail.com>
2024-09-14 13:31:26 -0700

    use torch.float32 rather than torch.float16

--- nnunetv2/inference/predict_from_raw_data.py
+++ nnunetv2/inference/predict_from_raw_data.py
@@ -600,9 +600,9 @@ class nnUNetPredictor(object):
             if self.verbose:
                 print(f'preallocating results arrays on device {results_device}')
             predicted_logits = torch.zeros((self.label_manager.num_segmentation_heads, *data.shape[1:]),
-                                           dtype=torch.half,
+                                           dtype=torch.float32,
                                            device=results_device)
-            n_predictions = torch.zeros(data.shape[1:], dtype=torch.half, device=results_device)
+            n_predictions = torch.zeros(data.shape[1:], dtype=torch.float32, device=results_device)
 
             if self.use_gaussian:
                 gaussian = compute_gaussian(tuple(self.configuration_manager.patch_size), sigma_scale=1. / 8,
